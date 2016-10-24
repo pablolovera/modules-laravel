380515b5638cdd719c07c950f0746ab3b1adbe1a
Pablo Lovera <pablolovera180@gmail.com>
2016-10-24 10:51:59 -0200

    namespaces changed and created abstract class for modules

---
+++ src/ModulesLaravel/Commands/BaseModules.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pablo
+ * Date: 24/10/16
+ * Time: 10:07
+ */
+
+namespace PabloLovera\ModulesLaravel\Commands;
+
+
+class BaseModules
+{
+
+}
---
+++ src/ModulesLaravel/Commands/ModuleViewList.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace PabloLovera\ModulesLaravel\Commands;
+
+use Illuminate\Console\Command;
+use PabloLovera\ModulesLaravel\Traits\CommandTrait;
+
+class ModuleViewLista extends Command
+{
+    use CommandTrait;
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'module:make-view-lista {module}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new List View';
+
+    /**
+     * The stub name
+     *
+     * @var string
+     * */
+    protected $stub = 'view-lista';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $moduleDirectory    = config('module.modules_directory');
+        $module             = $this->argument('module');
+        $toDirectory        = $moduleDirectory . $module . '/Views';
+
+        $content            = $this->getContents($this->stub);
+
+        $content            = $this->replaceModuleNameLower($module, $content);
+        $content            = $this->replaceCamelModuleName($module, $content);
+
+        $this->doDirectory($toDirectory);
+
+        $this->writeFile($content, $toDirectory, 'lista.blade');
+
+        $this->info('The Module ' . $module .' has been received a new list view. Be happy!');
+    }
+}
